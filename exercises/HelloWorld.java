ef77083e0bceb85abf62f0fb94935867a8eb18dd
jsvictorias <juliasantos7652@gmail.com>
2024-04-17 10:57:47 -0300

    the first challenge

---
+++ exercises/HelloWorld.java
@@ -0,0 +1,9 @@
+//The classical introductory exercise. Just say "Hello, World!".
+
+class Greeter {
+    String getGreeting(){
+        return "Hello World!";
+    }
+}
+
+//OPNIONS: very easy exercise, the classic.
