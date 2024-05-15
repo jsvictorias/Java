7a02e59f53f2c6cac564d7838888726fc551abf2
jsvictorias <juliasantos7652@gmail.com>
2024-05-15 17:31:49 -0300

    `feat:` new exercise

---
+++ exercises/CalculatorConundrum.java
@@ -0,0 +1,9 @@
+/*In this exercise you will be building error handling for a simple integer calculator.
+ To make matters simple, methods for calculating addition, multiplication and division are provided.
+
+The goal is to have a working calculator that returns a String with the following pattern: 16 + 51 = 67,
+ when provided with arguments 16, 51 and +. */
+
+
+
+// OPINION: 
